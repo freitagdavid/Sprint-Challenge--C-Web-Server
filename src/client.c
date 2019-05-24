8c2879ed3fa1fd5ea490fb9b5bd86dd02a056b55
David Freitag <freitagdavid@outlook.com>
2019-05-24 13:24:26 -0400

    client: Complete

--- src/client.c
+++ src/client.c
@@ -1,11 +1,11 @@
+#include "lib.h"
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
 #include <string.h>
-#include <sys/types.h>
 #include <sys/socket.h>
-#include "lib.h"
+#include <sys/types.h>
+#include <unistd.h>
 
 #define BUFSIZE 4096 // max number of bytes we can get at once
 
@@ -24,31 +24,36 @@ typedef struct urlinfo_t {
  * url: The input URL to parse.
  *
  * Store hostname, path, and port in a urlinfo_t struct and return the struct.
-*/
-urlinfo_t *parse_url(char *url)
-{
+ */
+urlinfo_t *parse_url(char *url) {
   // copy the input URL so as not to mutate the original
-  char *hostname = strdup(url);
-  char *port;
-  char *path;
-
   urlinfo_t *urlinfo = malloc(sizeof(urlinfo_t));
 
   /*
     We can parse the input URL by doing the following:
 
-    1. Use strchr to find the first slash in the URL (this is assuming there is no http:// or https:// in the URL).
+    1. Use strchr to find the first slash in the URL (this is assuming there is
+    no http:// or https:// in the URL).
     2. Set the path pointer to 1 character after the spot returned by strchr.
-    3. Overwrite the slash with a '\0' so that we are no longer considering anything after the slash.
+    3. Overwrite the slash with a '\0' so that we are no longer considering
+    anything after the slash.
     4. Use strchr to find the first colon in the URL.
     5. Set the port pointer to 1 character after the spot returned by strchr.
-    6. Overwrite the colon with a '\0' so that we are just left with the hostname.
+    6. Overwrite the colon with a '\0' so that we are just left with the
+    hostname.
   */
 
   ///////////////////
   // IMPLEMENT ME! //
   ///////////////////
-
+  char *current_token;
+  current_token = strchr(url, '/');
+  urlinfo->path = current_token + 1;
+  *current_token = '\0';
+  current_token = strchr(url, ':');
+  urlinfo->port = current_token + 1;
+  *current_token = '\0';
+  urlinfo->hostname = url;
   return urlinfo;
 }
 
@@ -61,9 +66,8 @@ urlinfo_t *parse_url(char *url)
  * path:     The path string.
  *
  * Return the value from the send() function.
-*/
-int send_request(int fd, char *hostname, char *port, char *path)
-{
+ */
+int send_request(int fd, char *hostname, char *port, char *path) {
   const int max_request_size = 16384;
   char request[max_request_size];
   int rv;
@@ -72,24 +76,43 @@ int send_request(int fd, char *hostname, char *port, char *path)
   // IMPLEMENT ME! //
   ///////////////////
 
-  return 0;
+  int request_length = sprintf(request,
+                               "GET /%s HTTP/1.1\n"
+                               "Host: %s\n"
+                               "Connection: keep-alive\n"
+                               "Accept: text/html\n"
+                               "\n",
+                               path, hostname);
+
+  printf("Request:\n    %s\n", request);
+
+  rv = send(fd, request, max_request_size, 0);
+
+  printf("%d\n", rv);
+
+  return rv;
 }
 
-int main(int argc, char *argv[])
-{  
-  int sockfd, numbytes;  
+int main(int argc, char *argv[]) {
+  int sockfd, numbytes;
   char buf[BUFSIZE];
 
   if (argc != 2) {
-    fprintf(stderr,"usage: client HOSTNAME:PORT/PATH\n");
+    fprintf(stderr, "usage: client HOSTNAME:PORT/PATH\n");
     exit(1);
   }
+  struct urlinfo_t *url = parse_url(argv[1]);
+  sockfd = get_socket(url->hostname, url->port);
+  send_request(sockfd, url->hostname, url->port, url->path);
 
+  numbytes = recv(sockfd, buf, BUFSIZE - 1, 0);
+  printf("%s", buf);
   /*
     1. Parse the input URL
     2. Initialize a socket by calling the `get_socket` function from lib.c
     3. Call `send_request` to construct the request and send it
-    4. Call `recv` in a loop until there is no more data to receive from the server. Print the received response to stdout.
+    4. Call `recv` in a loop until there is no more data to receive from the
+    server. Print the received response to stdout.
     5. Clean up any allocated memory and open file descriptors.
   */
 
